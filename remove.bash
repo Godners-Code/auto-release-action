d81074a6bda7d9e54bcd62e1c5bdbfe5d6be9898
TianYue Ren <godners@163.com>
2026-06-03 12:48:03 +0800

    Update remove.bash

--- remove.bash
+++ remove.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "[STEP] [$(date '+%F %T') (UTC)] Remove Existent Release via Bash Script"
-gh release delete ${{ steps.worker-info.outputs.tag }} --yes --cleanup-tag 2>/dev/null || echo ""
+gh release delete ${RELEASE_TAG} --yes --cleanup-tag 2>/dev/null || echo ""
